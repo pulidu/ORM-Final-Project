578e3a870546b196828ad4ad653e2ae4747d2cef
pulidu <godagepulindu@gmail.com>
2025-09-23 15:03:00 +0530

    update invalidcredentialsexception

---
+++ src/main/java/lk/ijse/orm_final/exception/InvalidCredentialsException.java
@@ -0,0 +1,8 @@
+package lk.ijse.orm_final.exception;
+
+public class InvalidCredentialsException extends RuntimeException {
+    public InvalidCredentialsException(String message) {
+
+        super(message);
+    }
+}
