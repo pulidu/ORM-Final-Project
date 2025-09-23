cccd50483486546499f092c263487b316819e875
pulidu <godagepulindu@gmail.com>
2025-09-23 15:00:47 +0530

    update exception handler java class

---
+++ src/main/java/lk/ijse/orm_final/exception/ExceptionHandler.java
@@ -0,0 +1,15 @@
+package lk.ijse.orm_final.exception;
+
+import javafx.scene.control.Alert;
+
+public class ExceptionHandler {
+
+
+    public static void handleException(Exception e) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText("An error occurred");
+        alert.setContentText(e.getMessage());
+        alert.showAndWait();
+    }
+}
