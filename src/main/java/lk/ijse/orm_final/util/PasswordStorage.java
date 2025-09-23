fd46884857f56da523af2d374e8ab17df38d7877
pulidu <godagepulindu@gmail.com>
2025-09-24 03:09:16 +0530

    update password storage java class

---
+++ src/main/java/lk/ijse/orm_final/util/PasswordStorage.java
@@ -0,0 +1,15 @@
+package lk.ijse.orm_final.util;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class PasswordStorage {
+
+    public static String hashPassword(String password) {
+
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+    public static boolean checkPassword(String inputPassword, String hashedPassword) {
+        return BCrypt.checkpw(inputPassword, hashedPassword);
+    }
+}
